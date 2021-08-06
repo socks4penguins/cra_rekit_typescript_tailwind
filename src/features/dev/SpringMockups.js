1f508288db09484378685907eada20818af83b8f
philip cleaver <socks4penguins@gmail.com>
2021-08-06 18:01:10 +0000

    mockups

--- src/features/dev/SpringMockups.js
+++ src/features/dev/SpringMockups.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from "react"
-import { useSpring, animated, to } from "@react-spring/web"
+import { useSpring, animated } from "@react-spring/web"
+
 import card2 from "../../images/mockup-card2.svg"
 import content from "../../images/mockup-content.svg"
 import bg2 from "../../images/mockup2-bg.svg"
@@ -9,8 +10,8 @@ import bg3 from "../../images/mockup3-bg.svg"
 
 export default function SpringMockups() {
   const [hover, setHover] = useState(false)
-  const logoTarget = useRef(null)
-  const bottom = useSpring({ bottom: hover ? "55%" : "50%" })
+  // const logoTarget = useRef(null)
+  // const top = useSpring({ top: hover ? "400px": "370px"   })
   const imageSize = useSpring({
     width: hover ? "48px" : "32px",
     height: hover ? "48px" : "32px",
@@ -19,48 +20,63 @@ export default function SpringMockups() {
   const images = [
     {
       url: card2,
-      background:
-        "radial-gradient(218.51% 281.09% at 100% 100%, rgba(253, 63, 51, 0.6) 0%, rgba(76, 0, 200, 0.6) 45.83%, rgba(76, 0, 200, 0.6) 100%)",
-      width: "183px",
-      height: "120px",
-      top: "150px",
-      left: "-37px",
+      style: {
+        background:
+          "radial-gradient(218.51% 281.09% at 100% 100%, rgba(253, 63, 51, 0.6) 0%, rgba(76, 0, 200, 0.6) 45.83%, rgba(76, 0, 200, 0.6) 100%)",
+        width: "183px",
+        height: "120px",
+        top: "150px",
+        left: "-37px",
+      },
+      hoverStyle: useSpring({ top: "110px", left: "-40px" }),
     },
     {
       url: card2,
-      background:
-        "linear-gradient(192.64deg, rgb(67, 22, 219) 12.72%, rgb(144, 118, 231) 54.49%, rgb(162, 238, 255) 100.01%)",
-      width: "183px",
-      height: "120px",
-      top: "150px",
-      left: "176px",
+      style: {
+        background:
+          "linear-gradient(192.64deg, rgb(67, 22, 219) 12.72%, rgb(144, 118, 231) 54.49%, rgb(162, 238, 255) 100.01%)",
+        width: "183px",
+        height: "120px",
+        top: "150px",
+        left: "176px",
+      },
+      hoverStyle: useSpring({ top: "110px", left: "190px" }),
     },
     {
       url: content,
-      background: "rgba(23, 12, 61, 0.3)",
-      width: "701px",
-      height: "428px",
-      top: "200px",
-      left: "0px",
-      backdropFilter: "blur(10px)",
+      style: {
+        background: "rgba(23, 12, 61, 0.3)",
+        width: "701px",
+        height: "428px",
+        top: "200px",
+        left: "0px",
+        backdropFilter: "blur(10px)",
+      },
+      hoverStyle: {},
     },
     {
       url: bg2,
-      background: "rgba(23, 12, 61, 0.2)",
-      width: "400px",
-      height: "273px",
-      top: "370px",
-      left: "40px",
-      backdropFilter: "blur(10px)",
+      style: {
+        background: "rgba(23, 12, 61, 0.2)",
+        width: "400px",
+        height: "273px",
+        top: "370px",
+        left: "40px",
+        backdropFilter: "blur(10px)",
+      },
+      hoverStyle: useSpring({ top: "400px", left: "-40px" }),
     },
     {
       url: bg3,
-      background: "rgba(23, 12, 61, 0.2)",
-      width: "414px",
-      height: "273px",
-      top: "400px",
-      left: "340px",
-      backdropFilter: "blur(10px)",
+      style: {
+        background: "rgba(23, 12, 61, 0.2)",
+        width: "414px",
+        height: "273px",
+        top: "400px",
+        left: "340px",
+        backdropFilter: "blur(10px)",
+      },
+      hoverStyle: useSpring({ left: "370px" }),
     },
   ]
 
@@ -74,19 +90,36 @@ export default function SpringMockups() {
       onMouseEnter={(e) => setHover(true)}
       onMouseLeave={(e) => setHover(false)}
     >
-      {images.map((img, index) => (
-        <img
-          key={index}
-          src={img.url}
-          alt=""
-          style={{
-            transition: "all 2s cubic-bezier(0.075, 0.82, 0.165, 1) 0s",
-            position: "absolute",
-            ...img,
-            ...(hover ? {} : tilt),
-          }}
-        />
-      ))}
+      {images.map((img, index) =>
+        // false ? (
+        //   <animated.img
+        //     key={index}
+        //     src={img.url}
+        //     alt=""
+        //     style={{
+        //       transition: "all 2s cubic-bezier(0.075, 0.82, 0.165, 1) 0s",
+        //       position: "absolute",
+        //       ...img.style,
+        //       ...(!hover ? {} : tilt),
+        //       // ...img.hoverStyle,
+        //     }}
+        //   />
+        // ) :
+          (
+          <animated.img
+            key={index}
+            src={img.url}
+            alt=""
+            style={{
+              transition: "all 2s cubic-bezier(0.075, 0.82, 0.165, 1) 0s",
+              position: "absolute",
+              ...img.style,
+              ...(hover ? img.hoverStyle : tilt),
+              
+            }}
+          />
+        )
+      )}
     </div>
   )
 }
